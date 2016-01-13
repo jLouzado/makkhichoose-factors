979e81b47a6b9ab3cb2ac51a7a225874bfdba11d
Joel Louzado <jlouzado@gmail.com>
2016-01-13 11:39:24 +0530

    Added form to accept number
    * no logic yet, just accepting the number and then displaying it out.

--- factors.py
+++ factors.py
@@ -1,10 +1,28 @@
+import cgi
 import webapp2
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/factors" method="post">
+      <div><input type="text" name="number"><br></div>
+      <div><input type="submit" value="Find Factors"></div>
+    </form>
+  </body>
+</html>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write(MAIN_PAGE_HTML)
+
+class Factors(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>Your number is: ')
+        self.response.write(cgi.escape(self.request.get('number')))
+        self.response.write('</body></html>')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/factors', Factors),
 ], debug=True)
